eaf764772cf213bd4abd3eed017f24c1f96a2293
Tingyu Yang <tingyuyang@lclark.edu>
2016-07-27 01:22:14 -0700

    Practice with subString in java

---
+++ subString.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        int X = 1234;
+        String s = String.valueOf(X);
+        System.out.println(s.substring(0,2));
+        
+     }
+
+}
+// how to compile in "http://www.tutorialspoint.com/compile_java_online.php"
