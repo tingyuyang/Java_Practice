20d8c5725e9268c397cd7975d3fdad3688f4267d
Tingyu Yang <tingyuyang@lclark.edu>
2016-07-27 01:41:46 -0700

    Update subString.java

--- subString.java
+++ subString.java
@@ -1,3 +1,4 @@
+//subString: http://www.tutorialspoint.com/java/java_string_substring.htm
 public class HelloWorld{
 
      public static void main(String []args){
@@ -9,12 +10,12 @@ public class HelloWorld{
      }
 
 }
+
 // how to compile in "http://www.tutorialspoint.com/compile_java_online.php"
 // Compile then Excute.
 // number: 1234
 // indent: 0123
 // Output is 12, "s.substring(0,2)" so it will be before indent 2
-
 
 //For the "One Two"
 //coz: One_Two
