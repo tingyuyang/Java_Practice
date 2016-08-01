c6e12c7be13ea0f62f90a92d8ed00e873fcbe85f
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-01 02:23:39 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,80 +7,83 @@ len=32
 */
 import java.util.*;
 import java.io.File;
-public class HelloWorld{
-    static int length = 0;
-    static int depth = 0;
+
+public class dir {
+  static int length = 0;
+  static int depth = 0;
   
-    public static void main(String[] args) throws Exception {
-        File root = new File("."); // current folder
-        showAllFiles(root);
-        System.out.println("len="+length);
-    }
+  public static void main(String[] args) throws Exception {
+    File root = new File("."); // current folder
+    showAllFiles(root);
+    System.out.println("len="+length);
+  }
 
-    final static void showAllFiles(File dir) throws Exception {
-        File[] fs = dir.listFiles();
+  final static void showAllFiles(File dir) throws Exception {
+    File[] fs = dir.listFiles();
+    
+    depth++;    
+    for (int i = 0; i < fs.length; i++) {
+      //for indent purpose  
+      if (depth > 1) {
+        for (int j = 0; j < depth - 1; j++) {
+          System.out.print(" ");
+        }
+      } 
         
-        depth++;    
-        for (int i = 0; i < fs.length; i++) {
-          //for indent purpose  
-          if (depth > 1) {
-            for (int j = 0; j < depth - 1; j++) {
-              System.out.print(" ");
-            }
-          } 
-            
-          System.out.println(fs[i].getName()+depth);
-          length = length + solution(fs[i].getAbsolutePath());
-          if (fs[i].isDirectory()) {
-            try {
-              showAllFiles(fs[i]);
-            } catch (Exception e) {
-            }
-          }
+      length = length + solution(fs[i].getAbsolutePath());
+      System.out.println(fs[i].getName());
+      if (fs[i].isDirectory()) {
+        try {
+          showAllFiles(fs[i]);
+        } catch (Exception e) {
         }
-        depth--;    
+      }
     }
+    depth--;    
+  }
 
-    static public int solution(String S) {
-        if (S.isEmpty()) {
-          return 0;
-        }
-        int sumPath = 0;
-        int currentLen = 0;
-        Stack<String> sta = new Stack<String>();
-        String[] lines = S.split("\n");
-        int len = lines.length; //# of directories and files
-        for (int i = 0; i < len; i++) {
-          while (!sta.isEmpty() &&
-                 (numOfSpace(lines[i]) <= numOfSpace(sta.peek()))) {
-            currentLen -= (sta.peek().length() - numOfSpace(sta.peek()) + 1);
-            sta.pop();
-          }
-          currentLen += (lines[i].length() - numOfSpace(lines[i]) + 1);
-          sta.push(lines[i]);
-          if (isImage(lines[i])) {
-            sumPath += currentLen;
-          }
-        }
-        return sumPath;
+  static public int solution(String S) {
+    if (S.isEmpty()) {
+      return 0;
     }
+    int sumPath = 0;
+    int currentLen = 0;
+    Stack<String> sta = new Stack<String>();
+    String[] lines = S.split("\n");
+    int len = lines.length; //# of directories and files
+    for (int i = 0; i < len; i++) {
+      while (!sta.isEmpty() &&
+             (numOfSpace(lines[i]) <= numOfSpace(sta.peek()))) {
+        currentLen -= (sta.peek().length() - numOfSpace(sta.peek()) + 1);
+        sta.pop();
+      }
+      currentLen += (lines[i].length() - numOfSpace(lines[i]) + 1);
+      sta.push(lines[i]);
+      if (isImage(lines[i])) {
+        System.out.println("debug:path="+S);
+        sumPath = S.length();
+      }
+    }
+    return sumPath;
+  }
 
-    static public int numOfSpace(String str) {
-        int num = 0;
-        int len = str.length();
-        for (int i = 0; i < len; i++) {
-          if (str.charAt(i) == ' ') {
-            num += 1;
-          }
-        }
-        return num;
+  static public int numOfSpace(String str) {
+    int num = 0;
+    int len = str.length();
+    for (int i = 0; i < len; i++) {
+      if (str.charAt(i) == ' ') {
+        num += 1;
+      }
     }
+    return num;
+  }
 
-    static boolean isImage(String str) {
-        if (str.endsWith(".jpeg") || str.endsWith(".png") || str.endsWith(".gif")) {
-          return true;
-        } else {
-          return false;
-        }
+  static boolean isImage(String str) {
+    if (str.endsWith(".jpeg") || str.endsWith(".png") || str.endsWith(".gif")) {
+      return true;
+    } else {
+      return false;
     }
+  }
 }
+
